959a3d0bbfd30e254e80d9eb03e39a0615c28a93
jabrown8 <j.a.browns.inbox@gmail.com>
2022-07-28 18:31:51 +0200

    added project folder

---
+++ java/lessons/src/net/ctdp/rfdynhud/widgets/lessons/project/project/hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package project;
+
+/**
+ * @author Jaybee
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
